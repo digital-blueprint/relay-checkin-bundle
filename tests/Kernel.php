708ac3897974bcff0b1c65de87bdbd576336b1f6
Christoph Reiter <reiter.christoph@gmail.com>
2021-07-20 12:00:09 +0200

    cs-fixer

--- tests/Kernel.php
+++ tests/Kernel.php
@@ -11,13 +11,13 @@ use DBP\API\LocationCheckInBundle\DbpLocationCheckInBundle;
 use Nelmio\CorsBundle\NelmioCorsBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Bundle\MonologBundle\MonologBundle;
 use Symfony\Bundle\SecurityBundle\SecurityBundle;
 use Symfony\Bundle\TwigBundle\TwigBundle;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
-use Symfony\Bundle\MonologBundle\MonologBundle;
 
 class Kernel extends BaseKernel
 {
